1a322a4b44a9f0990261f177becb2fc19e0bf84a
fxx <fancxxy@gmail.com>
2016-05-15 20:49:41 +0800

    add function to download favorite works

--- src/pixiv/PixivCpp.cpp
+++ src/pixiv/PixivCpp.cpp
@@ -1,10 +1,24 @@
 #include "../curl/CurlRequest.h"
 #include "PixivCpp.h"
-#include "ParseJson.h"
 
 #include <iostream> 
 #include <fstream> 
-#include <vector>
+#include <json/reader.h> 
+#include <json/writer.h> 
+
+Json::Value parseJson(const std::string &json) {
+    Json::Value root;
+    Json::Reader reader;
+    reader.parse(json, root);
+    return root;
+}
+
+void styleJson(const std::string &file_name, const std::string &json) {
+    Json::Value root = parseJson(json);
+    Json::StyledWriter writer;
+    std::fstream file(file_name, std::ios::out);
+    file <<  writer.write(root);
+}
 
 PixivCpp::PixivCpp()
 {
@@ -12,8 +26,7 @@ PixivCpp::PixivCpp()
 
     std::ifstream ifile(".pixiv", std::ios::in | std::ios::binary);
     if (ifile.good()) {
-        ifile >> username >> password;
-    } else {
+        ifile >> username >> password; } else {
         std::cout << "input username and password: ";
         std::cin >> username >> password;
 
@@ -43,10 +56,10 @@ void PixivCpp::PixivOauth(const std::string &username, const std::string &passwo
     
     CurlResponse response = request_.CurlPost(url, data, header);
 
-    access_token_ =  parseJson(response.text, "response", "access_token");
+    access_token_ =  parseJson(response.text)["response"]["access_token"].asString();
 }
 
-const std::string PixivCpp::works(const std::string &illust_id) {
+const std::string PixivCpp::GetWorks(const std::string &illust_id) {
     CurlUrl url = "https://public-api.secure.pixiv.net/v1/works/" + illust_id + ".json";
 
     CurlParams params = {
@@ -62,36 +75,83 @@ const std::string PixivCpp::works(const std::string &illust_id) {
     };
 
     CurlResponse response = request_.CurlGet(url, header, params);
+    styleJson(illust_id, response.text);
+    return response.text;
+}
+
+const std::string PixivCpp::GetFavorite(const std::string page, 
+        const std::string per_page, 
+        const std::string publicity, 
+        const std::string image_sizes) {
+    CurlUrl url = "https://public-api.secure.pixiv.net/v1/me/favorite_works.json";
+
+    CurlParams params = {
+        {"page", page},
+        {"per_page", per_page},
+        {"publicity", publicity},
+        {"image_sizes", image_sizes}
+    };
+
+    CurlHeader header = {
+        {"Referer", "http://www.pixiv.net/"},
+        {"Authorization", "Bearer " + access_token_}
+    };
+
+    CurlResponse response = request_.CurlGet(url, header, params);
     return response.text;
 }
 
 void PixivCpp::DownloadWorks(const std::string &illust_id) {
     CurlUrl original_url;
     std::string file_name;
-    std::string response_json = works(illust_id);            
+    std::string response = GetWorks(illust_id);            
 
-    //CurlUrl original_url = parseJson(response_json, "response", 0, "image_urls", "large");
-    Json::Value result = parseJson(response_json);
-    int page_count = std::stoi(result["response"][0]["page_count"].asString());
-
-    std::cout << "page_count: " << page_count << std::endl;
-    for (int i = 0; i < page_count; i++) {
-        if (page_count == 1)
-            original_url = result["response"][0]["image_urls"]["large"].asString();
-        else
-            original_url = result["response"][0]["metadata"]["pages"][i]["image_urls"]["large"].asString();
-
-        std::cout << i + 1 << ": " << original_url << std::endl;
-
-        file_name = original_url.substr(original_url.rfind("/")+1);
-        PixivDownload(original_url, file_name);
+    Json::Value json = parseJson(response);
+    if (json["status"] == "success") {
+        Json::Value works = json["response"][0];
+        PixivDownload(works);
     }
 }
 
-void PixivCpp::PixivDownload(const CurlUrl &url, const std::string &file_name) {
+void PixivCpp::DownloadFavorite(const std::string page, 
+        const std::string per_page, 
+        const std::string publicity, 
+        const std::string image_sizes) {
+    std::string response = GetFavorite(page, per_page, publicity, image_sizes);
+
+    Json::Value json = parseJson(response);
+    if (json["status"] == "success") {
+        for (int count = 0; count < json["count"].asInt(); count++) {
+            Json::Value favorite = json["response"][count]["work"];
+            PixivDownload(favorite);
+        }
+    }
+}
+
+void PixivCpp::PixivDownload(const Json::Value &root) {
+    CurlUrl url;
     CurlHeader header = {
         {"Referer", "http://www.pixiv.net/"},
     };
 
-    CurlResponse response = request_.CurlGet(url, header, {}, file_name);
+
+    std::string file_name;
+    int page_count = root["page_count"].asInt(); 
+    
+    std::cout << "id: " << root["id"].asString() << std::endl;
+    std::cout << "title: " << root["title"].asString() << std::endl;
+    std::cout << "caption: " << root["caption"].asString() << std::endl;
+    std::cout << "page_count: " << page_count << std::endl;
+
+    for (int i = 0; i < page_count; i++) {
+        if (page_count == 1)
+            url = root["image_urls"]["large"].asString();
+        else
+            url = root["metadata"]["pages"][i]["image_urls"]["large"].asString();
+
+        std::cout << i + 1 << ": " << url << std::endl;
+
+        file_name = url.substr(url.rfind("/")+1);
+        request_.CurlGet(url, header, {}, file_name);
+    }
 }
--- src/pixiv/PixivCpp.h
+++ src/pixiv/PixivCpp.h
@@ -1,20 +1,33 @@
 #pragma once
 
 #include "../curl/CurlRequest.h"
+
 #include <string> 
+#include <json/value.h> 
 
 class PixivCpp 
 { 
     public: 
         PixivCpp(); 
 
-        const std::string works(const std::string &illust_id);
         void DownloadWorks(const std::string &illust_id);
 
+        void DownloadFavorite(const std::string page = "1", 
+                const std::string per_page = "50", 
+                const std::string publicity = "public",
+                const std::string image_sizes = "px_128x128,px_480mw,large");
 
     private: 
         void PixivOauth(const std::string &username, const std::string &password);
-        void PixivDownload(const CurlUrl &url, const std::string &file_name);
+
+        void PixivDownload(const Json::Value&);
+
+        const std::string GetWorks(const std::string &illust_id);
+
+        const std::string GetFavorite(const std::string page = "1", 
+                const std::string per_page = "50", 
+                const std::string publicity = "public",
+                const std::string image_sizes = "px_128x128,px_480mw,large");
 
         CurlRequest request_;
         std::string access_token_;
